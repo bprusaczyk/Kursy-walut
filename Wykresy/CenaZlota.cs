5ea54ad502ee21bd0b80f712b526e640e628db2b
Bartosz Prusaczyk <37440663+bprusaczyk@users.noreply.github.com>
2020-02-26 16:42:36 +0100

    Create CenaZlota.cs

---
+++ Wykresy/CenaZlota.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Wykresy
+{
+    class CenaZlota
+    {
+        public string data { get; set; }
+        public double cena { get; set; }
+    }
+}
